1661212185e88efcc7886433d78f4d7cccd4a3eb
MarianaEsquivelHernandez <esquivelmariana0702@gmail.com>
2022-11-23 21:51:13 -0600

    Ready

--- Act5.2HashNodes/hashNodes.cpp
+++ Act5.2HashNodes/hashNodes.cpp
@@ -1,30 +1,46 @@
 /// Act 5.2 - Comprehensive activity on the use of hash codes (Competence evidence)
 /// Author: Juan Jose Salazar Cortes
 /// Author: Mariana Esquivel Hernandez
-/// date: 11/02/2022
+/// date: 11/23/2022
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <typeinfo>
+#include <queue>
+#include <map>
+#include <array>
+#include <list>
+#include <stdio.h>
+#include <string.h>
+#include <cstring>
 
 using namespace std;
 
 struct Node{
+    int numAccess;
     string IP;
-    int NumAccesos;
-    list <string> accesos;
-    list <string> puertos;
-    Node(string fechaYHora, string ip,string puerto){
-        this->IP=ip;
-        this->NumAccesos=1;
-        this->accesos.push_back(fechaYHora);
-        this->puertos.push_back(puerto);
+    list <string> ports;
+    list <string> access;
+    Node(string dataTime, string ip, string port){
+        this-> numAccess = 1;
+        this-> IP = ip;
+        this-> ports.push_back (port);
+        this-> access.push_back (dataTime);
     }
 };
 
-string getDayAndMonth(string record){
+// obtain basic info from the IP address
+string getDayMonth(string record){
     if(record[5] == ' ') return record.substr(0, 14);
     else return record.substr(0, 15);
 } // Time Complexity O(1)
 
+// take IP from line
 string getIP(string record){
     long long contEsp = 0;
     long long indexBeg;
@@ -48,6 +64,7 @@ string getIPAccess(string IP){
     return access;
 } // Time Complexity O(n)
 
+// Take the port of the IP
 string getIPPort(string IP){
     string access;
     for(int i = 14; i <= IP.length(); i++){
@@ -62,111 +79,104 @@ class Hashtable{
         list<Node> *table;
     public:
         Hashtable(){
-            this->size=16807;
-            table=new list<Node>[16807];
+            this-> size = 16807;
+            table = new list<Node>[16807];
         }
         int hashFunction(string ip);
         void insert(Node n);
-        void cargarDatos(string file);
+        void loadInfo(string file);
         void search(string ip);
         void print();
 };
+
+// Hash function 
 int Hashtable::hashFunction(string ip){    
-    //hashFunction:O(n)
-    //Hash function
-    int hash=0;
-    for(int i=0;i<ip.length();i++){
-        hash+=ip[i];
+    int hash = 0;
+    for(int i = 0; i < ip.length(); i++){
+        hash += ip[i];
     }
-    return hash%size;
-}
-void Hashtable::insert(Node n){
-    //insert:O(n)
-    //Quadratic probing overflow handling
-    int index=hashFunction(n.IP);
+    return hash % size;
+}// Time Complexity O(n)
+
+// Insert the node in the hash table and if the IP is already in the table, it will add the access to the list of accesses
+void Hashtable::insert(Node value){
+    int index = hashFunction(value.IP);
     if(table[index].empty()){
-        table[index].push_back(n);
+        table[index].push_back(value);
     }else{
-        int i=1;
-        while(!table[index].empty()||table[index].front().IP==n.IP){
-            if(table[index].front().IP==n.IP){
-                table[index].front().NumAccesos++;
-                table[index].front().accesos.push_back(n.accesos.front());
-                table[index].front().puertos.push_back(n.puertos.front());
+        int i = 1;
+        while (!table[index].empty() || table[index].front().IP == value.IP){
+            if (table[index].front().IP == value.IP){
+                table[index].front().ports.push_back(value.ports.front());
+                table[index].front().access.push_back(value.access.front());
+                table[index].front().numAccess ++;
                 return;
             }
-            index=(index+i*i)%size;
+            index = (index + i * i) % size;
             i++;
         }
-        table[index].push_back(n);
+        table[index].push_back(value);
     }
-}
+}// Time Complexity O(n)
 
-void Hashtable::cargarDatos(string file){
- //cargarDatos: O(n)
-    //Loads the data from the file into the heap
-    ifstream archivo;
-    archivo.open(file);
+// Load the data from the file and insert the nodes in the hash table
+void Hashtable::loadInfo(string file){
+    ifstream document;
+    document.open(file);
     string line;
-    while(getline(archivo,line)){
-        Node n(getDayAndMonth(line),getIPAccess(getIP(line)), getIPPort(getIP(line)));
-        insert(n);
+    while(getline(document,line)){
+        Node value(getDayMonth(line),getIPAccess(getIP(line)), getIPPort(getIP(line)));
+        insert(value);
     }
-    archivo.close();
-}
-void Hashtable::search(string ip){
-    //search: O(n)
-    //Searches for a node in the table
-    int index=hashFunction(ip);
-    if(table[index].empty()){
-        cout<<"No se encontro el registro"<<endl;
-    }else{
-        int i=1;
-        while(!table[index].empty()||table[index].front().IP==ip){
-            if(table[index].front().IP==ip){
-                cout << "IP: "<< table[index].front().IP <<endl;
-                cout << "Numero de accesos: " << table[index].front().NumAccesos << endl;
-                cout << "\n\t-- Accesos: --\n" <<endl;
-                for(auto it=table[index].front().accesos.begin();it!=table[index].front().accesos.end();it++){
-                    cout << "\t" << *it << endl;
+    document.close();
+}// Time Complexity O(n)
+
+// Search the IP in the hash table and print the information of the node
+void Hashtable::search (string ip){
+    int index = hashFunction(ip);
+    if (table[index].empty()){
+        cout << "Ip not in record" << endl;
+    } else {
+        int i = 1;
+        int j = 1;
+        while (!table[index].empty() || table[index].front().IP == ip){
+            if (table[index].front().IP == ip){
+                cout << "\nIP: " << table[index].front().IP << endl;
+                cout <<"\nNumber of access: "<<table[index].front().numAccess << endl;
+                cout <<"\nDay and Time of the access: " << endl;
+                for(auto it = table[index].front().access.begin(); it != table[index].front().access.end(); it ++){
+                    cout <<"Access " << j << ": \t"<<*it << endl;
+                    j++;
                 }
-                cout << "\n\t-- Puertos: --\n" << endl;
-                for(auto it=table[index].front().puertos.begin();it!=table[index].front().puertos.end();it++){
-                    cout << "\t*--- " << *it << " ---*" << endl;
+                j = 1;
+                cout<<"\nPort from access: "<<endl;
+                for(auto it = table[index].front().ports.begin(); it != table[index].front().ports.end(); it ++){
+                    cout <<"Access "<< j << ": "<< "\t*--- " << *it << " ---*" << endl;
+                    j++;
                 }
                 return;
             }
-            index=(index+i*i)%size;
+            index=(index + i * i) % size;
             i++;
         }
-        cout<<"No se encontro el registro"<<endl;
+        cout<<"Not in record"<<endl;
     }
-}
-
-void Hashtable::print(){
-    //print: O(n)
-    //Prints the table
-    for(int i=0;i<size;i++){
-        if(!table[i].empty()&&table[i].front().NumAccesos>1){
-            cout<<i<<": "<<table[i].front().IP<<" :::"<<table[i].front().NumAccesos<<endl;
-            cout<<"\tAccesos: "<<endl;
-            for(auto it=table[i].front().accesos.begin();it!=table[i].front().accesos.end();it++){
-                cout<<"\t\t"<<*it<<endl;
-            }
-            cout<<"\tPuertos: "<<endl;
-            for(auto it=table[i].front().puertos.begin();it!=table[i].front().puertos.end();it++){
-                cout<<"\t\t"<<*it<<endl;
-            }
-        }
-    }
-}
+}// Time Complexity O(n)
 
 int main(){
-    Hashtable h;
-    h.cargarDatos("bitacora.txt");
-    string ip;
-    cout<<"\nIngrese la IP a buscar: ";
-    cin >> ip;
-    h.search(ip);
+    fflush(stdin);
+    Hashtable Ips;
+    Ips.loadInfo("bitacora.txt");
+    string Ip;
+    do {
+        cout<< "\nWrite EXIT if you want to end the program" << endl;
+        cout << "\n----------------------------IP info-----------------------------" << endl;
+        cout << "\nIngresa la IP que buscas en el Siguiente Formato: " << endl;
+        cout << "Example: xxx.xx.xxx.xx "; //423.2.230.77
+        cin>>Ip;
+        if(Ip == "EXIT") break;
+        Ips.search(Ip);
+    }
+    while(Ip != "EXIT");
     return 0;
-}
+}// Time Complexity O(n)
